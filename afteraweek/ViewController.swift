628a6c009c62380e77bc4dc3cc450d502188099d
Shady Adel <shadyadel@Shadys-Mac.local>
2017-09-09 13:05:21 +0200

    Initial Commit

---
+++ afteraweek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  afteraweek
+//
+//  Created by Shady Adel on 9/9/17.
+//  Copyright © 2017 Shady Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
